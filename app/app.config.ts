7c1ac41556e96595c62388d626655985c41aee93
mehotkhan <github-action@actions-template-sync.noreply.github.com>
2025-03-02 13:56:25 +0000

    chore(template): merge template changes :up:
    Signed-off-by: mehotkhan <github-action@actions-template-sync.noreply.github.com>

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,112 +5,64 @@ export default defineAppConfig({
     },
   },
   mainMenu: [
-    [
-      {
-        label: "Home",
-        // icon: "i-lucide-home",
-        to: "/",
-      },
-      {
-        label: "Explorer",
-        // icon: "i-lucide-search",
-        to: "/explorer",
-      },
-      {
-        label: "About",
-        // icon: "i-lucide-info",
-        to: "/about",
-      },
-      {
-        label: "Contact",
-        // icon: "i-lucide-mail",
-        to: "/contact",
-      },
-    ],
-    [
-      {
-        label: "Manage",
-        slot: "manage",
-      },
-      {
-        label: "i18n",
-        slot: "i18n",
-      },
-      {
-        label: "theme",
-        slot: "theme",
-      },
-
-      {
-        label: "avatar",
-        slot: "avatar",
-      },
-    ],
+    {
+      label: "Home",
+      // icon: "i-lucide-home",
+      to: "/",
+    },
+    {
+      label: "Explorer",
+      // icon: "i-lucide-search",
+      to: "/explorer",
+    },
+    {
+      label: "About",
+      // icon: "i-lucide-info",
+      to: "/about",
+    },
+    {
+      label: "Contact",
+      // icon: "i-lucide-mail",
+      to: "/contact",
+    },
   ],
   dashMenu: [
-    [
-      {
-        label: "Dashboard",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage",
-      },
-      {
-        label: "Items",
-        // icon: "i-lucide-book-open",
-        // active: true,
-        defaultOpen: true,
-        to: "/manage/items",
-      },
-      // {
-      //   label: "Comments",
-      //   // icon: "i-lucide-database",
-      //   to: "/manage/comments",
-      // },
-      {
-        label: "Users",
-        // icon: "i-lucide-box",
-        to: "/manage/users",
-        defaultOpen: true,
-      },
-      {
-        label: "Builds",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage/builds",
-      },
-      {
-        label: "Settings",
-        // type: "label",
-        // icon: "i-lucide-book-open",
-        to: "/manage/settings",
-      },
-    ],
-    [
-      {
-        label: "Manage",
-        slot: "manage",
-      },
-      {
-        label: "Help",
-        // icon: "i-lucide-circle-help",
-        // disabled: true,
-      },
-      {
-        label: "i18n",
-        slot: "i18n",
-      },
-
-      {
-        label: "theme",
-        slot: "theme",
-      },
-
-      {
-        label: "avatar",
-        slot: "avatar",
-      },
-    ],
+    {
+      label: "Dashboard",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage",
+    },
+    {
+      label: "Items",
+      // icon: "i-lucide-book-open",
+      // active: true,
+      defaultOpen: true,
+      to: "/manage/items",
+    },
+    // {
+    //   label: "Comments",
+    //   // icon: "i-lucide-database",
+    //   to: "/manage/comments",
+    // },
+    {
+      label: "Users",
+      // icon: "i-lucide-box",
+      to: "/manage/users",
+      defaultOpen: true,
+    },
+    {
+      label: "Builds",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage/builds",
+    },
+    {
+      label: "Settings",
+      // type: "label",
+      // icon: "i-lucide-book-open",
+      to: "/manage/settings",
+    },
   ],
   corePermissions: [
     "dashboard.read",
@@ -142,13 +94,14 @@ export default defineAppConfig({
     "config.manage",
   ],
   app: {
-    site_name_fa: "بگ چه",
-    title_fa: "بگ چه",
+    site_name_fa: "مَموچی",
+    title_fa: "مَـ موچیــ",
     description_fa: "کوچولو و بازیگوش",
-    site_name_en: "Bagche",
-    title_en: "Bagche",
+    site_name_en: "Mamoochi",
+    title_en: "Ma moochi",
     description_en: "Tiny & Playful",
     footer_fa: "همه چیز به شکل شگفت انگیزی خوب باید باشد",
     footer_en: "Everything must be wonderful in every shape and form ",
   },
+  category: ["books", "notes", "projects"],
 });
